be76dc941cca99067735a30562a68a634263a91f
Neko110923 <neko110923@163.com>
2024-07-09 21:10:47 +0800

    Hello New StarsMod!

---
+++ src/main/java/cn/neko/starsmod/StarsMod.java
@@ -0,0 +1,10 @@
+package cn.neko.starsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StarsMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/cn/neko/starsmod/client/StarsModClient.java
@@ -0,0 +1,10 @@
+package cn.neko.starsmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StarsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
